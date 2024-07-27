89f67c22e6b39ed6ecd72dc16195593b0065ce1f
Sandeepraj97 <sandeep2111997@gmail.com>
2024-07-27 10:22:34 +0530

    commit message

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: ["Front End Developer", "Mern Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function openCertificate(certificate) {
+    window.open(certificate);
+}
+function opentab(certificate) {
+    window.open(certificate);
+}
